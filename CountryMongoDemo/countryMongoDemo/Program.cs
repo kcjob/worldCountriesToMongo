c3a4bd13f34c56c4bbf2435bfff3cff9737dc1e9
chris <p40000x@us.royalahold.net>
2022-12-02 11:50:04 -0500

    Initial commit

---
+++ CountryMongoDemo/countryMongoDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+using System.Text.Json;
+using CountryMongoDemo.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public static async Task Main()
+    {
+        await CountriesAsync();
+    }
+
+    static async Task CountriesAsync()
+    {
+        var apiURL = "https://restcountries.com/v3.1/all"; //alpha/tto";
+
+        var results = await client.GetStringAsync(apiURL);
+
+        string connectionString = "mongodb://localhost";
+        string databaseName = "world";
+        string collectionName = "countries";
+        MongoClient dbClient = new MongoClient(connectionString);
+        var database = dbClient.GetDatabase(databaseName);
+        var collection = database.GetCollection<CountryModel>(collectionName);
+
+        //------------------- using insertMany() -----------------------------
+        List<CountryModel> countryList = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<List<CountryModel>>(results);
+        collection.InsertMany(countryList);
+
+        foreach (var country in countryList)
+        {
+            Console.WriteLine(country);
+            //collection.InsertOne(country);
+            //Console.WriteLine($"OK?: {resultWrites.IsAcknowledged} - Inserted Count: {resultWrites.InsertedCount}");
+
+        }
+
+
+        //var countryList = JsonSerializer.Deserialize<List<CountryModel>>(results);
+        //Console.WriteLine(countryList[0].name.official);
+        //foreach (var country in countryList)
+        //{
+        //    Console.WriteLine(country.name.nativeName.eng.common);
+        //    Console.WriteLine(country.tld[0]);
+        //    Console.WriteLine(country.currencies.TTD.symbol);
+        //    Console.WriteLine(country.subregion);
+        //}
+
+        //var dbList = dbClient.ListDatabases().ToList();
+
+        //Console.WriteLine("The list of databases on this server is: ");
+        //foreach (var db in dbList)
+        //{
+        //    Console.WriteLine(db);
+        //}
+
+    }
+
+}
+
---
+++ CountryMongoDemo/countryMongoDemo/src/Models/CountryModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CountryMongoDemo.Models
+{
+
+    [BsonIgnoreExtraElements]
+    public class CountryModel
+	{
+        //[BsonId]
+        //[BsonRepresentation(BsonType.ObjectId)]
+        //public ObjectId _id { get; set; }
+        //[BsonElement("Name")]
+		public Name? name { get; set;}
+		public List<string>? tld { get; set; }
+		public string? cca2 { get; set; }
+        public string? ccn3 { get; set; }
+        public string? cca3 { get; set; }
+        public string? cioc { get; set; }
+        public bool independent { get; set; }
+        public string? status { get; set; }
+        public bool unMember { get; set; }
+        public Currencies? currencies { get; set; }
+        public Idd? idd { get; set; }
+        public List<string>? capital { get; set; }
+        public List<string>? altSpellings { get; set; }
+        public string? region { get; set; }
+        public string? subregion { get; set; }
+
+
+    }
+
+    [BsonIgnoreExtraElements]
+    public class Name
+	{
+		public string? common { get; set; }
+		public string? official { get; set; }
+		public NativeName? nativeName { get; set; }
+    }
+
+    [BsonIgnoreExtraElements]
+    public class NativeName
+    {
+		[BsonElement("eng")]
+		public Eng? eng { get; set; }
+	}
+
+    [BsonIgnoreExtraElements]
+    public class Eng
+	{
+		public string? official { get; set; }
+		public string? common { get; set; }
+	}
+
+    [BsonIgnoreExtraElements]
+    public class Currencies
+	{
+        [BsonElement("ttd")]
+        public TTD? TTD { get; set; }
+	}
+
+    [BsonIgnoreExtraElements]
+    public class TTD
+	{
+			public string? name { get; set; }
+			public string? symbol { get; set; }
+	}
+	
+    public class Idd
+    {
+        public string? root { get; set; }
+        public List<string>? suffixes { get; set; }
+
+    }
+}
+
